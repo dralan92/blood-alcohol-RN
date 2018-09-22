f2b173d07db957b60a2a7f7b5084ed7817990363
unknown <codename100.alan@gmail.com>
2018-09-22 02:19:35 -0400

    added style for login and register button

--- components/IndexPage.js
+++ components/IndexPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View ,ListView,TouchableOpacity,Button} from 'react-native';
+import { StyleSheet, Text, View ,ListView,TouchableOpacity,Button,TouchableHighlight} from 'react-native';
 
 class IndexPage extends Component {
     static navigationOptions = {
@@ -7,12 +7,55 @@ class IndexPage extends Component {
     }
   render() {
     return (
-        <View>
-            <Text>Index</Text>
-      </View>
+      
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+              }}>
+              <TouchableOpacity
+              style = {styles.login_button}
+              >
+              <Text style = {styles.login_button_text}>Login</Text>
+              </TouchableOpacity> 
+
+              <TouchableOpacity
+              style = {styles.register_button}
+              >
+              <Text style = {styles.login_button_text}>Register</Text>
+              </TouchableOpacity> 
+            </View>
     )
   }
 }
 
-
+const styles = StyleSheet.create({
+    login_button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+        backgroundColor : '#910f05',
+        position: "absolute",
+        top:150,
+        height:100,
+        width : '100%',
+        
+    },
+    register_button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+        backgroundColor : '#910f05',
+        position: "absolute",
+        top:350,
+        height:100,
+        width : '100%',
+        
+    },
+    login_button_text:{
+        color : 'white',
+        fontSize: 19,
+        fontWeight: 'bold',
+    }
+});
 export default IndexPage;
