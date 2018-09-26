d7fa6f3893f92166edf133d5dcb4b2eb18e8768e
unknown <codename100.alan@gmail.com>
2018-09-26 04:22:20 -0400

    POST for registration working, no endpoint to POST password, next i have to add that in my REST end point

--- components/Home.js
+++ components/Home.js
@@ -1,20 +1,96 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View ,ListView,TouchableOpacity,Button} from 'react-native';
-
+import firebaseApp from '../Firebase';
 
 class Home extends Component {
+  constructor(){
+    super();
+    this.drinkersRef = this.getRef().child('drinkers')
+    this.state = {
+      name : '',
+      email : '',
+      password : '',
+      gender : '',
+      weight : '',
+      age : '',
+      lob : 0,
+      goa : 0,
+      bac : 0
+
+    }
+  }
     static navigationOptions = {
         title: 'Home',
+    }
+   
+    getRef(){
+      return firebaseApp.database().ref();
+    }
+    updateGOA(){
+      prev_goa = this.state.goa;
+      new_goa = prev_goa + 17.01;
+      this.setState({goa : new_goa});
+    }
+    updateStates(name,email,password,gender,age,liters_of_blood,grams_of_alcohol){
+      console.log(email);
+      this.setState({
+        name : name,
+        email : email,
+        password : password,
+        gender : gender,
+
+        age : age,
+        liters_of_blood : liters_of_blood,
+        grams_of_alcohol : grams_of_alcohol,
+
+      });
     }
   render() {
     const { navigation } = this.props;
     const name = navigation.getParam('name', 'x');
+    const email = navigation.getParam('email', 'x');
+    const password = navigation.getParam('password', 'x');
+    const gender = navigation.getParam('gender', 'x');
+    const weight = navigation.getParam('weight', 'x');
+    const age = navigation.getParam('age', 'x');
+    
+    const liters_of_blood = (parseFloat(weight) * .07) / 1.060 ;
+    const grams_of_alcohol = this.state.goa;
+
     return (
-        <View>
+      <View>
         <Text>hello {name}</Text>
+        <Text>you have  {liters_of_blood} liters of blood</Text>
+
+        <TouchableOpacity
+              style = {styles.register_button}
+              onPress={() => {
+                              this.updateGOA();
+                              this.updateStates(name,email,password,gender,age,liters_of_blood,grams_of_alcohol);
+                              this.drinkersRef.push(this.state);
+                            }
+                      }
+              >
+              <Text style = {styles.login_button_text}>Drink Once</Text>
+        </TouchableOpacity>     
       </View>
     )
   }
 }
+styles = StyleSheet.create({
+
+  register_button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    backgroundColor : '#910f05',
+    
+   
+    height:60,
+    width : '100%',
+    
+},
+});
+
 
 export default Home;
--- components/Register.js
+++ components/Register.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View ,ListView,TouchableOpacity,Button,TextInput,KeyboardAvoidingView} from 'react-native';
-import firebaseApp from '../Firebase';
+
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 class Register extends Component {
   constructor(){
     super();
-    this.drinkersRef = this.getRef().child('drinkers')
+    
   }
 
     static navigationOptions = {
@@ -22,44 +22,71 @@ class Register extends Component {
       age : '',
 
   }
-  getRef(){
-    return firebaseApp.database().ref();
+  registerAction(){
+
+    const new_drinker = {
+      name : this.state.name,
+      email : this.state.email,
+      password : this.state.password,
+      gender : this.state.gender,
+      age : this.state.age,
+      weight : parseFloat(this.state.weight)
+
+    }
+  fetch('https://infinite-temple-91100.herokuapp.com/drinkers',{
+    method : 'POST',
+    headers : {
+      Accept : 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body : JSON.stringify(new_drinker)
+  })
+  .then(result=>{
+    console.log(result);
+    
+  })
+  .catch(err=>{
+    console.log(err);
+    
+  });
   }
   render() {
     return (
       <KeyboardAvoidingView behavior = 'padding' style = {styles.container}>
         
-        <TextInput style = {styles.input} underlineColorAndroid='transparent' placeholder='Name' value={this.state.name}
+        <TextInput  style = {styles.input} underlineColorAndroid='transparent' placeholder='Name' value={this.state.name}
                     onChangeText={(text) => this.setState({name: text})}/>
-        <TextInput style = {styles.input} underlineColorAndroid='transparent' placeholder='Email' value={this.state.email}
+        <TextInput  style = {styles.input} underlineColorAndroid='transparent' placeholder='Email' value={this.state.email}
                     onChangeText={(text) => this.setState({email: text})}/>
-        <TextInput style = {styles.input} underlineColorAndroid='transparent' placeholder='Password' value={this.state.password}
+        <TextInput  style = {styles.input} underlineColorAndroid='transparent' placeholder='Password' value={this.state.password}
                     onChangeText={(text) => this.setState({password: text})}/>
-        <TextInput style = {styles.input} underlineColorAndroid='transparent' placeholder='Gender' value={this.state.gender}
+        <TextInput  style = {styles.input} underlineColorAndroid='transparent' placeholder='Gender' value={this.state.gender}
                     onChangeText={(text) => this.setState({gender: text})}/>
-        <TextInput style = {styles.input} underlineColorAndroid='transparent' placeholder='Weight in Kg' value={this.state.weight}
+        <TextInput  style = {styles.input} underlineColorAndroid='transparent' placeholder='Weight in Kg' value={this.state.weight}
                     onChangeText={(text) => this.setState({weight: text})}/>
-        <TextInput style = {styles.input} underlineColorAndroid='transparent' placeholder='Age' value={this.state.age}
+        <TextInput  style = {styles.input} underlineColorAndroid='transparent' placeholder='Age' value={this.state.age}
                     onChangeText={(text) => this.setState({age: text})}/>   
         <TouchableOpacity
               style = {styles.register_button}
-              onPress={() => {this.drinkersRef.push(this.state);
-                            this.props.navigation.navigate('Home',this.state)}}
+              onPress={() => {
+                            //this.props.navigation.navigate('Home',this.state);
+                            this.registerAction();
+                              }
+                          
+                      }
               >
-              <Text style = {styles.login_button_text}>Register</Text>
+              <Text style = {styles.button_text} >Register</Text>
         </TouchableOpacity>                      
       </KeyboardAvoidingView>
     )
   }
 }
 
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container : {
-    flex : 1,
-    padding : 20,
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-around',
-
   },
   input : {
     height : 40,
@@ -68,22 +95,19 @@ styles = StyleSheet.create({
     color : '#ffffff',
     paddingHorizontal : 10,
   },
-  register_button: {
+  register_button : {
     alignItems: 'center',
     justifyContent: 'center',
-    
     backgroundColor : '#910f05',
-    
-   
     height:60,
-    width : '100%',
-    
-},
-login_button_text:{
-  color : 'white',
+    width : '100%'
+  },
+  button_text : {
+    color : 'white',
   fontSize: 15,
-  fontWeight: 'bold',
-},
+  fontWeight: 'bold'
+  }
+
 });
 
 export default Register;
